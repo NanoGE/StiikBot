547eca3c848531db61b7a517faa90a3506f75e74
NanoGE <43835080+NanoGE@users.noreply.github.com>
2021-04-15 19:15:58 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.fill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(102, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(686, 399);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://stiick.xyz//register", System.UriKind.Absolute);
+            // 
+            // fill
+            // 
+            this.fill.Location = new System.Drawing.Point(12, 22);
+            this.fill.Name = "fill";
+            this.fill.Size = new System.Drawing.Size(75, 23);
+            this.fill.TabIndex = 1;
+            this.fill.Text = "Fill";
+            this.fill.UseVisualStyleBackColor = true;
+            this.fill.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fill);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button fill;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    namespace WindowsFormsApp3
+    {
+        public partial class Form1 : Form
+        {
+          
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                webBrowser1.Navigate("https://stiick.xyz//register");
+                
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+            webBrowser1.Document.GetElementById("username").SetAttribute("value", "sneak");
+            webBrowser1.Document.GetElementById("email").SetAttribute("value", "hcoy4@wimsg.com");
+            webBrowser1.Document.GetElementById("password").SetAttribute("value", "lol123");
+            webBrowser1.Document.GetElementById("confirm_password").SetAttribute("value", "lol123");
+            webBrowser1.Document.GetElementById("gender").SetAttribute("value", "male");
+            
+
+        }
+            
+        }
+
+    }
